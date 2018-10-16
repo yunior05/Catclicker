64574939402db413dfa03358bb1e2be4d68da15b
yun05 <yunior05@github.com>
2018-10-16 10:28:11 -0400

    Add function Hidden
    Optimized the code with the function Hidden.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-var cats = document.getElementsByClassName('cat');
-for (var i = cats.length - 1; i >= 0; i--) {
+var hidden = function() {
+	for (var i = cats.length - 1; i >= 0; i--) {
 	cats[i].classList.add("hidden");
-};
+};};
+var cats = document.getElementsByClassName('cat');
+hidden();
 
 var list = document.getElementsByClassName('list');
 
 for (var i = 0; i <= list.length; i++) {
 	list[i].addEventListener("click", (function(number, cats){
 		return function(){
-			for (var i = cats.length - 1; i >= 0; i--) {
-				cats[i].classList.add("hidden");
-			};
+			hidden();
 			cats[number].classList.remove("hidden");
 		}
 	})(i, cats));
