f46a2fbe389593bc3162ded7516f80f1584d4e88
yun05 <yunior05@github.com>
2018-10-16 10:56:52 -0400

    Start to implement model MVC
    Add model, views and controller in the script, remove DOM element from
    HTML.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,85 @@
-var hidden = function() {
-	for (var i = cats.length - 1; i >= 0; i--) {
-	cats[i].classList.add("hidden");
-};};
-var cats = document.getElementsByClassName('cat');
-hidden();
+$(function() {
+	var model = {
+		init: function(){
+			if (!cats){
+				cats = [
+				{
+					name: "Sam",
+					countClicks: 0,
+					url: "../images/cat.jpg"
+				},
+				{
+					name: "Dam",
+					countClicks: 0,
+					url: "../images/cat_2.jpg"
+				},
+				{
+					name: "Jayce",
+					countClicks: 0,
+					url: "../images/cat_3.jpg"
+				},
+				{
+					name: "Camerot",
+					countClicks: 0,
+					url: "../images/cat_4.webp"
+				},
+				{
+					name: "BrotherLyes",
+					countClicks: 0,
+					url: "../images/cat_5.jpg"
+				}
+				];
+			};
+		},
 
-var list = document.getElementsByClassName('list');
+		increment: function(){};
 
-for (var i = 0; i <= list.length; i++) {
-	list[i].addEventListener("click", (function(number, cats){
-		return function(){
-			hidden();
-			cats[number].classList.remove("hidden");
-		}
-	})(i, cats));
 
-	count = document.getElementsByTagName('p');
-	function myFunction(n) {
-		var number = count[n].innerHTML;
-		number++;
-		count[n].innerHTML = number;
 	};
+
+	var Controller = {
+		init: function(){
+			model.init();
+			view.init();
+		},
+
+		detect: function(){};
+	};
+
+	var view_list = {
+		init: function(){};
+	};
+
+	var view_cat = {
+		init: function(){};
+	}
+})
+
+
+// var hidden = function() {
+// 	for (var i = cats.length - 1; i >= 0; i--) {
+// 	cats[i].classList.add("hidden");
+// };};
+// var cats = document.getElementsByClassName('cat');
+// hidden();
+
+// var list = document.getElementsByClassName('list');
+
+// for (var i = 0; i <= list.length; i++) {
+// 	list[i].addEventListener("click", (function(number, cats){
+// 		return function(){
+// 			hidden();
+// 			cats[number].classList.remove("hidden");
+// 		}
+// 	})(i, cats));
+
+// 	count = document.getElementsByTagName('p');
+// 	function myFunction(n) {
+// 		var number = count[n].innerHTML;
+// 		number++;
+// 		count[n].innerHTML = number;
+// 	};
 		
-};
+// };
 
 
