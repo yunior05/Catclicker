ced58479364afe23cd4d78d1927c683ad98ea524
yun05 <yunior05@github.com>
2018-10-17 11:12:48 -0400

    Complete the basic operation
    Added all the missing functions, in the views and controller

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
 $(function(){
 	var model = {
 		init: function(){
+			//Create array "cats" with five objects of cats
 			cats = [
 				{
 					name: "Sam",
 					countClicks: 0,
-					url: "../images/cat.jpg"
+					url: "images/cat.jpg"
 				},
 				{
 					name: "Dam",
 					countClicks: 0,
-					url: "../images/cat_2.jpg"
+					url: "images/cat_2.jpg"
 				},
 				{
 					name: "Jayce",
 					countClicks: 0,
-					url: "../images/cat_3.jpg"
+					url: "images/cat_3.jpg"
 				},
 				{
 					name: "Camerot",
 					countClicks: 0,
-					url: "../images/cat_4.webp"
+					url: "images/cat_4.webp"
 				},
 				{
 					name: "BrotherLyes",
 					countClicks: 0,
-					url: "../images/cat_5.jpg"
+					url: "images/cat_5.jpg"
 				}
 				]
 		},
-
+		//return cats array
 		get_all_cats: function(){
 			return cats;
 		}
-
-
 	};
 
 	var controller = {
@@ -42,7 +41,7 @@ $(function(){
 			model.init();
 			view_list.init();
 		},
-
+		//call model to return all cats array
 		get_cats: function(){
 			return model.get_all_cats();
 		}
@@ -50,46 +49,51 @@ $(function(){
 
 	var view_list = {
 		init: function(){
+			catlist = $(".cat-list");
+			view_list.render();
+			
+		},
+
+		render: function(){
+			//Add to ul. cat-list all li elment 
 			Html = '';
 			controller.get_cats().forEach(function(cat){
-				Html += '<li class="cat">' + cat.name + '</li>';
-				$(".cat-list").html(Html);
+				Html += '<li class="cat"><a href="#">' + cat.name + '</a></li>';
+				catlist.html(Html);
 			})
+			list = $(".cat");
+			//When click in li element, call view_cat
+			for (var i = 0; i <= list.length; i++) {
+				list[i].addEventListener("click", (function(id){
+					return function(){
+					view_cat.init(id, controller.get_cats());
+					}
+				})(i));
+			};
 		}
 	};
 
 	var view_cat = {
-		init: function(){}
+		init: function(id, cats){
+			currentCat = id;
+			box = $(".cat-container");
+			view_cat.render()
+		},
+		render: function(){
+			//add to div.cat-container the image, name and count of clicks, for this cats
+			Html = '';
+			Html += '<span class="name">' + cats[currentCat].name + 
+			'</span>'+'<img class="cat-image" src="'+cats[currentCat].url+'"><br>'+cats[currentCat].countClicks + ' clicks';
+			box.html(Html);
+			//if click in the image, increment 1 to count variable, and render new information
+			$("img.cat-image")[0].addEventListener("click", function(){
+				cats[currentCat].countClicks++;
+				view_cat.render();
+			});
+		}
 	};
 
 	controller.init();
 });
-
-
-// var hidden = function() {
-// 	for (var i = cats.length - 1; i >= 0; i--) {
-// 	cats[i].classList.add("hidden");
-// };};
-// var cats = document.getElementsByClassName('cat');
-// hidden();
-
-// var list = document.getElementsByClassName('list');
-
-// for (var i = 0; i <= list.length; i++) {
-// 	list[i].addEventListener("click", (function(number, cats){
-// 		return function(){
-// 			hidden();
-// 			cats[number].classList.remove("hidden");
-// 		}
-// 	})(i, cats));
-
-// 	count = document.getElementsByTagName('p');
-// 	function myFunction(n) {
-// 		var number = count[n].innerHTML;
-// 		number++;
-// 		count[n].innerHTML = number;
-// 	};
-		
-// };
 
 
